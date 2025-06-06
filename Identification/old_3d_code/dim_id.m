bb6d6cc544f60d0b4bbc59bc836b0e70ddae39d8
Andrew Sykes <drewsykes28@gmail.com>
2025-06-06 15:19:57 -0700

    Purged redundant files and kept singular working detector

--- Identification/old_3d_code/dim_id.m
+++
@@ -1,9 +0,0 @@
-file = './staged_data/downscaling/downscaled_hdf5/DownScan_202.hdf5';
-dataset = '/RawData/Scan_202';
-
-info = h5info(file, dataset);
-dims = info.Dataspace.Size;
-
-disp(['Dataset dimensions: ' num2str(dims)]);
-
-% Dataset dimensions: 512  612  360
