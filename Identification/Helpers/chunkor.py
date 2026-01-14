130e60cfe36e056cc9d5e7c93131c84869c54a84
labuserpeshkov <drewsykes28@gmail.com>
2026-01-14 12:20:29 -0800

    Made chunker effectively pad data produced

--- Identification/Helpers/chunkor.py
+++ Identification/Helpers/chunkor.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import pandas as pd
+import torch.nn.functional as F
 from IPython.display import display
 
 def chunkor(data,kernel):
@@ -25,23 +26,28 @@ def chunkor(data,kernel):
     df = pd.DataFrame([np.round(item/(1024**3),2) for item in info],
                       index=['Total','Reserved','Allocated'],columns=['VRam'])
 
+    assert all(i % 2 == 1 for i in kernel.shape), 'Kernel dimensions must be odd!'
+    assert all(i % 2 == 0 for i in data.shape), 'Data dimensions must be even!'
+
     print(f'{client} device detected: {device}')
     print(f'Data Ram: {np.round(data.nbytes/(1024**3),2)} GB')
     print(f'Attempting {2**cuts} partitions')
     display(df)
 
     r,c,z = data.shape
-    pad = int(kernel.shape[0]/2)
+    pads = int(kernel.shape[0]/2)
 
-    lower = data[:,:,0:int(z/2)+pad] # Lower Half
-    upper = data[:,:,int(z/2)-pad:z] # Upper Half
+    lower = data[:,:,0:int(z/2)+pads] # Lower Half
+    upper = data[:,:,int(z/2)-pads:z] # Upper Half
 
     if cuts==1:
         chunks = [lower,upper]
     elif cuts == 2:
-        chunks = [lower[:,0:int(c/2)+pad,:],
-                  lower[:,int(c/2)-pad:c,:],
-                  upper[:,0:int(c/2)+pad,:],
-                  upper[:,int(c/2)-pad:c,:]]
+        chunks = [lower[:,0:int(c/2)+pads,:],
+                  lower[:,int(c/2)-pads:c,:],
+                  upper[:,0:int(c/2)+pads,:],
+                  upper[:,int(c/2)-pads:c,:]]
     
-    return chunks
+    pchunks = [F.pad(torch.from_numpy(chunk), pad=(pads,)*6) for chunk in chunks]
+
+    return pchunks
