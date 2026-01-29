1420147955863a22945da69183e8083425734c5a
labuserpeshkov <drewsykes28@gmail.com>
2026-01-29 11:57:04 -0800

    Routine reorganization

--- Identification/Legacy/pgrid3D.m
+++ Identification/Legacy/pgrid3D.m
@@ -1,31 +1,31 @@
-function [cr, over] = pgrid3D(spx, spy, spz, Nx, Ny, Nz, Np, radLen, rad)
+function [cr, overlap] = pgrid3D(xPeak, yPeak, zPeak, xLen, yLen, Nz, Np, radLen, rad)
 % pgrid3D  Create local grids around each particle and overlap matrix
 %
 % rad==1: cr = scalar distance
 % rad==0: cr = struct with x,y,z vector differences
 
 % Initialize
-over = zeros(Nx, Ny, Nz);
-if isempty(spx)
+overlap = zeros(xLen, yLen, Nz);
+if isempty(xPeak)
     if rad
-        cr = zeros(Nx, Ny, Nz);
+        cr = zeros(xLen, yLen, Nz);
     else
         cr = struct();
-        cr.x = zeros(Nx, Ny, Nz);
-        cr.y = zeros(Nx, Ny, Nz);
-        cr.z = zeros(Nx, Ny, Nz);
+        cr.x = zeros(xLen, yLen, Nz);
+        cr.y = zeros(xLen, yLen, Nz);
+        cr.z = zeros(xLen, yLen, Nz);
     end
     return;
 end
 
 % Initialize cr
 if rad
-    cr = max([Nx, Ny, Nz]) * ones(Nx, Ny, Nz);
+    cr = max([xLen, yLen, Nz]) * ones(xLen, yLen, Nz);
 else
     cr = struct();
-    cr.x = zeros(Nx, Ny, Nz);
-    cr.y = zeros(Nx, Ny, Nz);
-    cr.z = zeros(Nx, Ny, Nz);
+    cr.x = zeros(xLen, yLen, Nz);
+    cr.y = zeros(xLen, yLen, Nz);
+    cr.z = zeros(xLen, yLen, Nz);
 end
 
 llRange = -radLen:radLen;
@@ -33,9 +33,9 @@ kkRange = -radLen:radLen;
 mmRange = -radLen:radLen;
 
 for np = 1:Np
-    ll = round(spx(np)) + llRange; ll = ll(ll >= 1 & ll <= Nx);
-    kk = round(spy(np)) + kkRange; kk = kk(kk >= 1 & kk <= Ny);
-    mm = round(spz(np)) + mmRange; mm = mm(mm >= 1 & mm <= Nz);
+    ll = round(xPeak(np)) + llRange; ll = ll(ll >= 1 & ll <= xLen);
+    kk = round(yPeak(np)) + kkRange; kk = kk(kk >= 1 & kk <= yLen);
+    mm = round(zPeak(np)) + mmRange; mm = mm(mm >= 1 & mm <= Nz);
     
     if isempty(ll) || isempty(kk) || isempty(mm)
         continue;
@@ -44,22 +44,22 @@ for np = 1:Np
     [XX, YY, ZZ] = ndgrid(ll, kk, mm);
     
     if rad
-        X = sqrt((XX - spx(np)).^2 + (YY - spy(np)).^2 + (ZZ - spz(np)).^2);
+        X = sqrt((XX - xPeak(np)).^2 + (YY - yPeak(np)).^2 + (ZZ - zPeak(np)).^2);
         mask = cr(ll, kk, mm) >= X;
         ind = find(mask);
-        over(sub2ind([Nx, Ny, Nz], XX(ind), YY(ind), ZZ(ind))) = np;
-        cr(sub2ind([Nx, Ny, Nz], XX(ind), YY(ind), ZZ(ind))) = X(ind);
+        overlap(sub2ind([xLen, yLen, Nz], XX(ind), YY(ind), ZZ(ind))) = np;
+        cr(sub2ind([xLen, yLen, Nz], XX(ind), YY(ind), ZZ(ind))) = X(ind);
     else
-        crx = XX - spx(np);
-        cry = YY - spy(np);
-        crz = ZZ - spz(np);
+        crx = XX - xPeak(np);
+        cry = YY - yPeak(np);
+        crz = ZZ - zPeak(np);
         dist = sqrt(crx.^2 + cry.^2 + crz.^2);
         mask = sqrt(cr.x(ll,kk,mm).^2 + cr.y(ll,kk,mm).^2 + cr.z(ll,kk,mm).^2) >= dist;
         ind = find(mask);
-        over(sub2ind([Nx, Ny, Nz], XX(ind), YY(ind), ZZ(ind))) = np;
-        cr.x(sub2ind([Nx, Ny, Nz], XX(ind), YY(ind), ZZ(ind))) = crx(ind);
-        cr.y(sub2ind([Nx, Ny, Nz], XX(ind), YY(ind), ZZ(ind))) = cry(ind);
-        cr.z(sub2ind([Nx, Ny, Nz], XX(ind), YY(ind), ZZ(ind))) = crz(ind);
+        overlap(sub2ind([xLen, yLen, Nz], XX(ind), YY(ind), ZZ(ind))) = np;
+        cr.x(sub2ind([xLen, yLen, Nz], XX(ind), YY(ind), ZZ(ind))) = crx(ind);
+        cr.y(sub2ind([xLen, yLen, Nz], XX(ind), YY(ind), ZZ(ind))) = cry(ind);
+        cr.z(sub2ind([xLen, yLen, Nz], XX(ind), YY(ind), ZZ(ind))) = crz(ind);
     end
 end
 end
